e547bdc9c0c5fb412330d3a31e6f283f89096fd4
DaShaun <dashaun@dashaun.com>
2024-05-22 05:39:03 -0400

    first pass

---
+++ src/test/java/io/dashaun/service/webcam/TestApplication.java
@@ -0,0 +1,14 @@
+package io.dashaun.service.webcam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestApplication.class).run(args);
+	}
+
+}
